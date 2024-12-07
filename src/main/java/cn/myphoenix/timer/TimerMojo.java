55d56b20d28f75e52cd02971731aa76ec27cb7d0
Legende_L <55222653+Acura-bit@users.noreply.github.com>
2024-12-07 11:36:08 +0800

    feat: timer有属性

--- src/main/java/cn/myphoenix/timer/TimerMojo.java
+++ src/main/java/cn/myphoenix/timer/TimerMojo.java
@@ -5,7 +5,9 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
@@ -17,7 +19,21 @@ import java.time.LocalDateTime;
  **/
 @Mojo(name = "timer", defaultPhase = LifecyclePhase.VALIDATE)
 public class TimerMojo extends AbstractMojo {
+
+    /**
+     * timeType = dateTime，则控制台显式日期时间
+     * timeType = date，则控制台只显示日期
+     */
+    @Parameter(property = "timeType", defaultValue = "dateTime")
+    private String timeType;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
-        this.getLog().info("现在是北京时间：" + LocalDateTime.now());
+        if ("dateTime".equals(timeType)) {
+            this.getLog().info("现在是北京时间：" + LocalDateTime.now());
+        } else if ("date".equals(timeType)) {
+            this.getLog().info("现在是：" + LocalDate.now());
+        } else {
+            this.getLog().info("现在是北京时间：" + LocalDateTime.now());
+        }
     }
 }
